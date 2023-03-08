5be0ed55b5e404e31e2fd941b57a8d940fa4905d
Mohand Soluman <MSoluman@getgroup.com>
2023-03-08 11:17:34 +0200

    start-section-3

---
+++ 03-gess-my-number-game/script.js
@@ -0,0 +1,7 @@
+'use strict';
+document.querySelector('.score').textContent = 13;
+document.querySelector('.number').textContent = 20;
+
+document.querySelector('.highscore').textContent = 5;
+let inp = (document.querySelector('.guess').value = 14);
+console.log(inp);
